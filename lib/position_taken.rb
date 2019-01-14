b3a618cb87490c869685c205e1cc92b29d5da9ab
richardtam.ny <richardtam.ny@gmail.com>
2019-01-14 20:21:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
-  if board[index] == "X" || "O"
+def position_taken?(array, index)
+  if array[index] == "X" || "O"
     true
-  elsif board[index] == " " || "" || nil
+  elsif array[index] == " " || "" || nil
     false
   end
 end
