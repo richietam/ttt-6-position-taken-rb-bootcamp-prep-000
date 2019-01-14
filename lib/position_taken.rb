91655ad484a5fc79a7e35f8f1c4cb3b2f7032e5b
richardtam.ny <richardtam.ny@gmail.com>
2019-01-14 20:30:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@
 def position_taken?(array, index)
   if array[index] == " " || array[index] == "" || array[index] == nil
     false
-  elsif array[index] == "X"
-    true
-  elsif array[index] == "O"
+  elsif array[index] == "X" || array[index] == "O"
     true
   end
 end
